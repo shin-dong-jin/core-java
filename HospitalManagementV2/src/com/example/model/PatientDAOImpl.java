f790772057cbed2a60e247dde72fd9d27e0b5e77
shin-dong-jin <y2kf9a0q@gmail.com>
2024-07-26 17:16:27 +0900

    [refactor]PatientDAO

--- HospitalManagementV2/src/com/example/model/PatientDAOImpl.java
+++ HospitalManagementV2/src/com/example/model/PatientDAOImpl.java
@@ -10,16 +10,11 @@ import java.util.List;
 public class PatientDAOImpl implements PatientDAO {
     private Connection connection;
 
-    public PatientDAOImpl() {
-        DBConnection dbConnection = new DBConnection();
-        this.connection = dbConnection.getConnection(); // 1 2 3
-    }
-
     @Override
     public boolean createPatient(PatientVO patient) throws SQLException {
-        DBConnection dbConnection = new DBConnection();
-        this.connection = dbConnection.getConnection(); // 1 2 3
+        connection = new DBConnection().getConnection(); // 1 2 3
         Statement statement = connection.createStatement(); // 4
+
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("INSERT INTO patient ")
                         .append("VALUES(")
@@ -32,22 +27,23 @@ public class PatientDAOImpl implements PatientDAO {
                         .append(patient.getHospitalFee()).append(",")
                         .append(patient.getMoney()).append(");");
 
-        int su = statement.executeUpdate(queryBuilder.toString()); // 5
+        int result = statement.executeUpdate(queryBuilder.toString()); // 5
         DBClose.dbClose(connection, statement);
-        return su == 1;
+        return result == 1;
     }
 
     @Override
     public PatientVO readPatient(int patientNumber) throws SQLException {
-        DBConnection dbConnection = new DBConnection();
-        this.connection = dbConnection.getConnection();
+        connection = new DBConnection().getConnection();
         Statement statement = connection.createStatement();
+
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("SELECT * FROM patient WHERE number = ").append(patientNumber).append(";");
         ResultSet resultSet = statement.executeQuery(queryBuilder.toString());
         if (!resultSet.next()) {
             return null;
         }
+
         PatientVO patient = new PatientVO(resultSet.getInt("number"),resultSet.getString("code"),
                 resultSet.getInt("days"), resultSet.getInt("age"));
         patient.setDept(resultSet.getString("dept"));
@@ -60,13 +56,18 @@ public class PatientDAOImpl implements PatientDAO {
 
     @Override
     public List<PatientVO> readAllPatients() throws SQLException {
-        DBConnection dbConnection = new DBConnection();
-        this.connection = dbConnection.getConnection();
+        connection = new DBConnection().getConnection();
         Statement statement = connection.createStatement();
         List<PatientVO> patients = new ArrayList<>();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM patient ORDER BY number;");
         while (resultSet.next()) {
-            patients.add(readPatient(resultSet.getInt("number")));
+            PatientVO patient = new PatientVO(resultSet.getInt("number"), resultSet.getString("code"),
+                                                resultSet.getInt("days"), resultSet.getInt("age"));
+            patient.setDept(resultSet.getString("dept"));
+            patient.setOperFee(resultSet.getInt("operfee"));
+            patient.setHospitalFee(resultSet.getInt("hospitalfee"));
+            patient.setMoney(resultSet.getInt("money"));
+            patients.add(patient);
         }
         DBClose.dbClose(connection, statement);
         return patients;
